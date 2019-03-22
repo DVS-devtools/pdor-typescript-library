f5997ec7c8a115b46b61b6c0b2f3827f1cb88403
Davide Gheri <davide.gheri@docomodigital.com>
2019-03-22 16:58:55 +0100

    new webpack config

--- webpack/paths.js
+++ webpack/paths.js
@@ -1,9 +1,24 @@
 const path = require('path');
+const fs = require('fs');
+
+const root = path.resolve(__dirname, '../');
+const entryPath = path.resolve(root, 'src', 'index.ts');
+const outputPath = path.resolve(root, 'dist');
+
+const browserEntry = () => {
+    const indexBrowser = path.resolve('../', 'index.browser.ts');
+    if (fs.existsSync(indexBrowser)) {
+        return indexBrowser;
+    }
+    return entryPath;
+};
 
 module.exports = {
-    root: path.resolve(__dirname, '../'),
-    outputPath: path.resolve(__dirname, '../', 'dist'),
-    entryPath: path.resolve(__dirname, '../', 'src/index.ts'),
+    root,
+    outputPath,
+    entryPath,
+    browserEntryPath: browserEntry(),
+    chunkFilename: '[name].chunk.[chunkhash:8].js',
     examplePath: {
         html: path.resolve(__dirname, '../', 'example/index.html'),
         js: path.resolve(__dirname, '../', 'example/index.ts'),
